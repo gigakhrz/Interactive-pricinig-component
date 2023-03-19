8fe89aeddc157f98130e007d7e3a547ea584c28d
giga kharazishvili <kharazishvilig13@gmail.com>
2023-03-19 14:11:58 -0700

    debugging is done

--- script.js
+++ script.js
@@ -18,33 +18,43 @@ rangeInput.addEventListener("input", () => {
     if(switchInput.checked===false && rangeInput.value==1){
         viwes.innerHTML = "10k pageviwes";
         priceToBuy.innerHTML = '$8.00';
+        duration.innerHTML = "/month";
     } else if(switchInput.checked===false && rangeInput.value==2){
         viwes.innerHTML = "50k pageviwes";
         priceToBuy.innerHTML = '$12.00';
+        duration.innerHTML = "/month";
     } else if(switchInput.checked===false && rangeInput.value==3){
         viwes.innerHTML = "100k pageviwes";
         priceToBuy.innerHTML = '$16.00';
+        duration.innerHTML = "/month";
     } else if(switchInput.checked===false && rangeInput.value==4){
         viwes.innerHTML = "500k pageviwes";
         priceToBuy.innerHTML = '$24.00';
+        duration.innerHTML = "/month";
     } else if(switchInput.checked===false && rangeInput.value==5){
         viwes.innerHTML = "1m pageviwes";
         priceToBuy.innerHTML = '$36.00';
+        duration.innerHTML = "/month";
     } else if(switchInput.checked===true && rangeInput.value==1){
         viwes.innerHTML = "10k pageviwes";
-        priceToBuy.innerHTML = `$${8*12*0.75}` 
+        priceToBuy.innerHTML = `$${8*12*0.75}`;
+        duration.innerHTML = "/year";
     } else if(switchInput.checked===true && rangeInput.value==2){
         viwes.innerHTML = "50k pageviwes";
-        priceToBuy.innerHTML = `$${12*12*0.75}` 
+        priceToBuy.innerHTML = `$${12*12*0.75}`;
+        duration.innerHTML = "/year"; 
     } else if(switchInput.checked===true && rangeInput.value==3){
         viwes.innerHTML = "100k pageviwes";
-        priceToBuy.innerHTML = `$${16*12*0.75}` 
+        priceToBuy.innerHTML = `$${16*12*0.75}`;
+        duration.innerHTML = "/year";
     } else if(switchInput.checked===true && rangeInput.value==4){
         viwes.innerHTML = "500k pageviwes";
-        priceToBuy.innerHTML = `$${24*12*0.75}` 
+        priceToBuy.innerHTML = `$${24*12*0.75}`;
+        duration.innerHTML = "/year";
     } else if(switchInput.checked===true && rangeInput.value==5){
         viwes.innerHTML = "10k pageviwes";
-        priceToBuy.innerHTML = `$${36*12*0.75}` 
+        priceToBuy.innerHTML = `$${36*12*0.75}`;
+        duration.innerHTML = "/year";
     } 
     
 })
